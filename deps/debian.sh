2256bd7c180a277f7614ecb4ab57e8858442ff45
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2018-04-08 15:31:27 +0000

    Init.

---
+++ deps/debian.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Script that installs native dependencies on Debian GNU/Linux.
+
+# You need root permissions to run this script.
+if [[ "${UID}" != '0' ]]; then
+    echo '> You need to become root to run this script.'
+    exit 1
+fi
+
+apt-get update
+apt-get install -y \
+    python3 \
+    python3-tornado
+
