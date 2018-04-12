a1d2b7806666f367ee72abbff6a83ba8a5582041
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2018-04-12 10:49:09 +0000

    Prototyping.

--- openvpn_status_ws_handler.py
+++ openvpn_status_ws_handler.py
@@ -2,16 +2,20 @@
 # -*- coding: utf-8 -*-
 import os
 from tornado.websocket import WebSocketHandler, WebSocketClosedError
+from time import sleep
+from openvpn_status_ws_helper import get_status_log_path_for_node
 
 class OpenvpnStatusWsHandler(WebSocketHandler):
     subscribers = set()
+    status_log_path = None
     timestamp = None
-
+    
     def check_origin(self, origin):
         return True
 
     def open(self, node):
-        self.node = node
+        self.status_log_path = get_status_log_path_for_node(node)
+        print(self.status_log_path)
         OpenvpnStatusWsHandler.subscribers.add(self)
 
     def on_message(self, message):
@@ -29,3 +33,4 @@ class OpenvpnStatusWsHandler(WebSocketHandler):
         if mtime != self.timestamp:
             self.timestamp = mtime
             self.write_message('Changed.')
+        sleep(1)
--- openvpn_status_ws_helper.py
+++ openvpn_status_ws_helper.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import os
+import json
 
 def parse_status_log(status_log_path):
     with open(status_log_path, 'r') as file_handle:
         print(file_handle.read())
+
+def get_status_log_path_for_node(node_id):
+    container = os.path.dirname(os.path.realpath(__file__))
+
+    if not container.endswith('/'):
+        container += '/'
+    settings_path = container + 'settings.json'
+
+    if not os.path.isfile(settings_path):
+        return False
+
+    with open(settings_path, 'r') as file_handle:
+        data = json.load(file_handle)
+
+        if data and 'nodes' in data:
+            nodes = data['nodes']
+        else:
+            return False
+
+        for node in nodes:
+            if int(node['id']) == int(node_id) and node['path']:
+                return node['path'].strip()
+
+    return False
