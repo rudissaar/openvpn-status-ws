193b6dfe5d73e4ab1c0183333700f5eedc6fa229
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2018-04-17 12:40:23 +0000

    Applied linter rules + minor fixes.

--- openvpn_status_ws.py
+++ openvpn_status_ws.py
@@ -1,50 +1,47 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import os
+
+"""Bootstrap file and entry point."""
+
 import signal
 import time
 import logging
-from tornado.options import define, parse_command_line, options
+
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
-from tornado.websocket import WebSocketHandler, WebSocketClosedError
-from tornado.web import Application
-from collections import defaultdict
+from tornado.options import define, parse_command_line, options
+
 import openvpn_status_ws_helper as helper
-import json
-
-
 from openvpn_status_ws_application import OpenvpnStatusWsApplication
 
-define('debug', default=False, type=bool, help='Run in debug mode')
-define('port', default=12200, type=int, help='Server port')
-define('address', default='0.0.0.0', help='Server address')
-define('allowed_hosts', default="localhost:12200", multiple=True,
-       help='Allowed hosts for cross domain connections')
-
+define('debug', default=False, type=bool, help='Run in debug mode.')
+define('port', default=12200, type=int, help='Server port.')
+define('address', default='0.0.0.0', help='Server address.')
 
 def shutdown(server, application):
+    """Stops application and server."""
     application.running = False
     ioloop = IOLoop.instance()
     logging.info('> Stopping server.')
     server.stop()
 
     def finalize():
-        ioloop.stop
+        """Makes sure that script will be terminated."""
+        ioloop.stop()
         logging.info('> Stopped.')
-        exit(0)
 
     ioloop.add_timeout(time.time() + 1, finalize)
 
 parse_command_line()
-application = OpenvpnStatusWsApplication(debug=options.debug)
-server = HTTPServer(application)
-server.listen(options.port, address=options.address)
-signal.signal(signal.SIGINT, lambda sig, frame: shutdown(server, application))
-logging.info('Starting server on {}:{}'.format(options.address, options.port))
+APPLICATION = OpenvpnStatusWsApplication(debug=options.debug)
+SERVER = HTTPServer(APPLICATION)
+SERVER.listen(options.port, address=options.address)
+signal.signal(signal.SIGINT, lambda sig, frame: shutdown(SERVER, APPLICATION))
+logging.info('Starting server on %s:%s', options.address, options.port)
 
-nodes = helper.get_nodes()
-for node in nodes:
-    application.watcher(node)
+NODES = helper.get_nodes()
+
+for node in NODES:
+    APPLICATION.watcher(node)
 
 IOLoop.instance().start()
