0f7fdf861d8af4e95790742790fcd2a084ed1f93
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2018-04-18 12:37:26 +0000

    Added method that returns settings file as dict to helper class.

--- openvpn_status_ws_helper.py
+++ openvpn_status_ws_helper.py
@@ -15,6 +15,16 @@ def get_settings_path():
 
     return settings_path
 
+def get_settings_dict():
+    """Return contents of the settings file as dict, None upon failure."""
+    settings_path = get_settings_path()
+
+    with open(settings_path, 'r') as file_handle:
+        data = json.load(file_handle)
+        return data
+
+    return None
+
 def get_nodes():
     nodes = list()
     settings_path = get_settings_path()
@@ -39,25 +49,15 @@ def parse_status_log(status_log_path):
     return False
 
 def get_status_log_path_for_node(node_id):
-    container = os.path.dirname(os.path.realpath(__file__))
+    data = get_settings_dict()
 
-    if not container.endswith('/'):
-        container += '/'
-    settings_path = container + 'settings.json'
-
-    if not os.path.isfile(settings_path):
+    if data and 'nodes' in data:
+        nodes = data['nodes']
+    else:
         return None
 
-    with open(settings_path, 'r') as file_handle:
-        data = json.load(file_handle)
-
-        if data and 'nodes' in data:
-            nodes = data['nodes']
-        else:
-            return None
-
-        for node in nodes:
-            if int(node['id']) == int(node_id) and node['path']:
-                return node['path'].strip()
+    for node in nodes:
+        if int(node['id']) == int(node_id) and node['path']:
+            return node['path'].strip()
 
     return None
