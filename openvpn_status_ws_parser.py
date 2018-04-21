af4c1d4b0fe46d5c7f4325af57bad8b77e899bfc
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2018-04-21 22:42:42 +0000

    OpenvpnStatusWsParser development.

--- openvpn_status_ws_parser.py
+++ openvpn_status_ws_parser.py
@@ -19,11 +19,44 @@ class OpenvpnStatusWsParser():
             self.raw_data = file_handle.read().strip()
 
     def fetch_log_type(self):
-        pass
+        first_line = self.raw_data.split("\n")[0].strip()
+        
+        if first_line == 'OpenVPN CLIENT LIST':
+            self.log_type = 'subnet'
+        elif first_line == 'OpenVPN STATISTICS':
+            self.log_type = 'ptp'
+
+    def get_clients(self):
+        lines = self.raw_data.split("\n")
+        start = lines.index('Common Name,Real Address,Bytes Received,Bytes Sent,Connected Since')
+        end = lines.index('ROUTING TABLE')
+
+        headers = ['common_name', 'real_address', 'bytes_received', 'bytes_sent', 'connected_since']
+        clients = dict()
+
+        for line in lines[start + 1:end]:
+            client_row = line.strip().split(',')
+            client = dict()
+
+            for index, header in enumerate(headers):
+                client[header] = client_row[index]
+
+            clients[client['common_name']] = client
+         
+        return clients
+
+    def get_clients_connected(self):
+        return len(self.get_clients())
 
     @property
     def data(self):
         data = dict()
-        data['raw_data'] = self.raw_data
+
+        if self.log_type == 'subnet':
+            data['topology'] = self.log_type
+            data['clients'] = self.get_clients()
+            data['clients_connected'] = self.get_clients_connected()
+        elif self.log_type == 'ptp':
+            data['topology'] = self.log_type
 
         return data
