637324b6acd70c28d358104bfd4ea52cdf853144
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2018-05-03 13:01:36 +0000

    Added file that you can use to change user and group for service file.

--- systemd/build-service-file.sh
+++ systemd/build-service-file.sh
@@ -4,6 +4,10 @@ RELATIVE_PATH=$(dirname ${0})
 SRC_FILE="${RELATIVE_PATH}/openvpn-status-ws.service.sample"
 DIST_FILE="${RELATIVE_PATH}/openvpn-status-ws.service"
 
+if [[ -f "${RELATIVE_PATH}/build-service-file.conf" ]]; then
+    source "${RELATIVE_PATH}/build-service-file.conf"
+fi
+
 if [[ -f "${SRC_FILE}" ]]; then
     scp "${SRC_FILE}" "${DIST_FILE}"
     sed -i "s/{{{USER}}}/"${USER}"/g" "${DIST_FILE}"
@@ -15,3 +19,4 @@ if [[ -f "${SRC_FILE}" ]]; then
 else
     echo '> Unable to find sample file.'
 fi
+
